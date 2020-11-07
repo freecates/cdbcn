1eb1c870eb3102b82ea991643a75f80afde6aba6
Ramon Gil <ramon.gil@deporvillage.com>
2020-11-07 20:01:35 +0100

    master , new landing with wideo and xxss

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,57 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Colla Castellers de Barcelona</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <h1 className={styles.title}>
+                <a href='https://www.castellersdebarcelona.cat/'>
+                    <Image
+                        width='600'
+                        height='300'
+                        loading='lazy'
+                        alt={'Logo de la Colla Castellers de Barcelona'}
+                        src={'/logo-castellers-de-barcelona.png'}
+                    />
+                </a>
+            </h1>
+            <video className={styles.video} loop muted autoPlay width='1680' height='945'>
+                <source src='https://cdbdata.now.sh/static/3d9.mp4' type='video/mp4' />
+                <source src='https://cdbdata.now.sh/static/3d9.webm' type='video/webm' />
+            </video>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <p className={styles.description}>
+                        <strong>Colla Castellers de Barcelona</strong>
+                        <br />
+                        [La Colla Degaga de la Ciutat]
+                        <br />
+                        <br />
+                        Carrer de Bilbao, 212 - 214 08018, Barcelona
+                        <br />
+                        T. 93 498 27 28
+                        <br />
+                        www.castellersdebarcelona.cat
+                        <br />
+                        <a href={'mailto:colla@castellersdebarcelona.cat'}>
+                            colla@castellersdebarcelona.cat
+                        </a>
+                    </p>
+                </main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+                <footer className={styles.footer}>
+                    <a href='https://www.instagram.com/castellersdebarcelona/'>Instagram</a>
+                    <a href='https://www.youtube.com/user/arxiucdb'>Youtube</a>
+                    <a href='https://twitter.com/cdbcn'>Twitter</a>
+                    <a href='https://www.facebook.com/castellersdebarcelona'>Facebook</a>
+                </footer>
+            </div>
+        </>
+    );
 }
