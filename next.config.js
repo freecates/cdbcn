a5086fa0b5b6a514d821da738bf875afc1f98168
Ramon Gil <ramon.gil@deporvillage.com>
2021-01-26 17:49:46 +0100

    master , added locale and default locale to config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     images: {
         domains: ['cdbdata.vercel.app', 'cms.castellersdebarcelona.cat'],
     },
+    i18n: {
+        locales: ['ca'],
+        defaultLocale: 'ca',
+    },
 };
