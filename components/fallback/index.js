1095615af62cda9c505b649e689e1013031770f1
Ramon Gil <ramon.gil@deporvillage.com>
2021-02-08 12:45:04 +0100

    master , added title to FallBack not found

--- components/fallback/index.js
+++ components/fallback/index.js
@@ -1,12 +1,21 @@
 import Layout from '@components/layout';
-import styles from './Fallback.module.scss'
+import styles from './Fallback.module.scss';
 
 const Fallback = ({ notFound }) => {
     return (
-        <Layout>
+        <Layout
+            title={notFound ? '404' : '... carregant'}
+            pageTitle={
+                notFound ? 'Pàgina no trobada o no disponible' : '... Estem carregant la pàgina'
+            }
+        >
             <div className={`${styles.fallBackComponent}`}>
                 <div className={`file${notFound ? '' : ' loading'}`}>
-                    <h1>{notFound ? 'Pàgina no trobada o no disponible en aquest moment' : '... Carregant'}</h1>
+                    <h1>
+                        {notFound
+                            ? 'Pàgina no trobada o no disponible en aquest moment'
+                            : '... Carregant'}
+                    </h1>
                 </div>
                 <style jsx>{`
                     .loading {
