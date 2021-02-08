1c6e247f2b034b0b8ae8141df3f9b1057ad60ed5
Ramon Gil <ramon.gil@deporvillage.com>
2021-02-08 12:14:11 +0100

    master , no supporters null

--- components/supporters/index.js
+++ components/supporters/index.js
@@ -3,7 +3,7 @@ import styles from './Supporters.module.scss';
 const staticDataUrl = `https://cdbdata.vercel.app/static`;
 
 const Supporters = ({ supporters }) => {
-    return (
+    return supporters && supporters.length ? (
         <div className={styles.supportersComponent}>
             {supporters.map((s, i) => (
                 <div key={s.type} className={styles.supporter}>
@@ -24,7 +24,7 @@ const Supporters = ({ supporters }) => {
                 </div>
             ))}
         </div>
-    );
+    ) : null;
 };
 
 export default Supporters;
