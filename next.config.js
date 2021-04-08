e0481828fe277bffb4f9a3895210985182ffea18
Ramon Gil <ramon.gil@deporvillage.com>
2021-04-08 12:47:55 +0200

    with-pwa added pwa next plugin

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
     images: {
-        domains: ['cdbdata.vercel.app', 'cms.castellersdebarcelona.cat', 'i.ytimg.com', 'live.staticflickr.com'],
+        domains: [
+            'cdbdata.vercel.app',
+            'cms.castellersdebarcelona.cat',
+            'i.ytimg.com',
+            'live.staticflickr.com',
+        ],
     },
     i18n: {
         locales: ['ca'],
         defaultLocale: 'ca',
     },
     future: {
-      webpack5: true,
+        webpack5: true,
     },
-};
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
+});
