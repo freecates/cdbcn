e498e3040fec1a0d92a478d06ba199f9aac7e781
Ramon Gil <ramon.gil@deporvillage.com>
2021-04-30 08:41:42 +0200

    with-latest-nextjs , webpack and pwa not in dev

--- next.config.js
+++ next.config.js
@@ -17,5 +17,9 @@ module.exports = withPWA({
     pwa: {
         dest: 'public',
         runtimeCaching,
+        disable: process.env.NODE_ENV === 'development',
+    },
+    future: {
+      webpack5: true,
     },
 });
