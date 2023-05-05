fcfa1ca5a5ac1696402d870f85e18f7aa795ce34
Ramon Gil <ramon.gil@deporvillage.com>
2023-05-05 14:16:17 +0200

    perf(cdbcn): [with-next-13-4] Added latest nextjs 13.4 stable app router

--- next.config.js
+++ next.config.js
@@ -14,39 +14,51 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 
 module.exports = withPWA({
     images: {
-        domains: [
-            'cdbdata.vercel.app',
-            'cms.castellersdebarcelona.cat',
-            'i.ytimg.com',
-            'live.staticflickr.com',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdbdata.vercel.app',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cms.castellersdebarcelona.cat',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.ytimg.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'live.staticflickr.com',
+            },
         ],
     },
     swcMinify: true,
     experimental: {
         appDir: true,
-      },
-      async redirects() {
+    },
+    async redirects() {
         return [
-          {
-            source: '/8-de-juny-de-1969',
-            destination: '/historia/8-de-juny-de-1969',
-            permanent: true,
-          },
-          {
-            source: '/53-anys-53-tuits',
-            destination: '/historia/53-anys-53-tuits',
-            permanent: true,
-          },
-          {
-            source: '/una-colla-singular-i-pionera',
-            destination: '/historia/una-colla-singular-i-pionera',
-            permanent: true,
-          },
-          {
-            source: '/ca/videos/:id',
-            destination: '/videos/:id',
-            permanent: true,
-          },
-        ]
-      },
+            {
+                source: '/8-de-juny-de-1969',
+                destination: '/historia/8-de-juny-de-1969',
+                permanent: true,
+            },
+            {
+                source: '/53-anys-53-tuits',
+                destination: '/historia/53-anys-53-tuits',
+                permanent: true,
+            },
+            {
+                source: '/una-colla-singular-i-pionera',
+                destination: '/historia/una-colla-singular-i-pionera',
+                permanent: true,
+            },
+            {
+                source: '/ca/videos/:id',
+                destination: '/videos/:id',
+                permanent: true,
+            },
+        ];
+    },
 });
